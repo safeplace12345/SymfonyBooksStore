e2cbc2506817e3044fec2742e5945f3e7e90e0f0
gh0st <safeplace12345@gmail.com>
2024-04-03 18:27:01 +0300

    added booking controller

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -1,18 +1,21 @@
 <?php
 
-namespace APP\Controller;
+namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
-class BooksController extends AbstractController {
-
-    #[Route('/books', name: "books", methods: ['GET', 'HEAD'])]
-    public function index(): Response {
+class BooksController extends AbstractController
+{
+    /**
+     * @Route("/books/{name}", name="app_books")
+     */
+    public function index($name): JsonResponse
+    {
         return $this->json([
-            "message" => "Welcome",
-            "Params" => "Params"
+            'message' => 'Welcome '. $name.  ' to your new controller!',
+            'path' => 'src/Controller/BooksController.php',
         ]);
     }
 }
